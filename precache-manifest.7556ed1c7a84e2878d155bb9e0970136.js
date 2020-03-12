39afa662414b549908a9ca86c09fc31c18b7b0da
Naga Vemprala <naga.vemprala@utsa.edu>
2020-03-12 13:55:52 -0500

    Updates

---
+++ precache-manifest.7556ed1c7a84e2878d155bb9e0970136.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0504c073e1fdd281f5c45458896508d9",
+    "url": "/Blockchain-based-review/index.html"
+  },
+  {
+    "revision": "44444e8557fd367721f1",
+    "url": "/Blockchain-based-review/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "5bd22c5c3a4a0079e6ac",
+    "url": "/Blockchain-based-review/static/js/2.b3774db6.chunk.js"
+  },
+  {
+    "revision": "1bc752fc7e467510a29e154020622760",
+    "url": "/Blockchain-based-review/static/js/2.b3774db6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "44444e8557fd367721f1",
+    "url": "/Blockchain-based-review/static/js/main.50beac23.chunk.js"
+  },
+  {
+    "revision": "4e2735629729a94c10b0",
+    "url": "/Blockchain-based-review/static/js/runtime-main.96f80f9b.js"
+  }
+]);
